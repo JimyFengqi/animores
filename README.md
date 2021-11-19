43763fe1d89eb577c3b68e99cee759ffe8b1227d
JimyFengqi <jmps515@163.com>
2021-11-19 15:12:34 +0800

    'deploy'

---
+++ README.md
@@ -0,0 +1,47 @@
+﻿**本项目源于：[https://github.com/jirengu-inc/animating-resume](https://github.com/jirengu-inc/animating-resume "https://github.com/jirengu-inc/animating-resume")。向作者表示深深的敬意。**
+
+# 一个会动的简历模板
+- 功能还很少，欢迎各位给我提意见和建议~
+
+## 线上预览
+
+> **This is my resume ➡️ [https://jimyfengqi.github.io/animores/index.html](https://jimyfengqi.github.io/animores/index.html)**
+
+## 项目截图
+
+[![枫奇丶宛南の个人简历](./screenshots/all_mini.png "枫奇丶宛南の个人简历")](https://jimyfengqi.github.io/animores/index.html "枫奇丶宛南の个人简历")
+
+## 使用方法
+```bash
+git clone https://github.com/jimyfengqi/animores.git
+cd animores
+npm install
+npm run dev
+```
+
+## 部署方法
+
+1. 编辑 **config/index.js**，修改第 10 行的 `assetsPublicPath`，值为 `项目名/public`。如果你没有修改项目名 `animores`，则可跳过此步骤。
+
+2. 编译、上传
+```bash
+npm run build
+git add .
+git commit -m "update"
+git push
+```
+3. 开启 **GitHub Pages** 功能，把生成的目标文件`public/*`放在你的web服务器上。
+
+4. 访问地址：[https://your-github-username.github.io/animores/public](https://jimyfengqi.github.io/animores/index.html "https://jimyfengqi.github.io/animores/index.html")
+
+# 关于我
+- [个人博客](https://jimyfengqi.github.io/)
+- [GitHub](https://github.com/jimyfengqi)
+- [码云](https://gitee.com/jimyfengqi)
+- [博客园](https://www.cnblogs.com/aifengqi/)
+- [CSDN](https://blog.csdn.net/qiqiyingse)
+- **主要涉及技术：`python后端开发`、`python爬虫`、`开源爱好者`、`Linux`**
+
+## License
+
+[Apache License](./LICENSE)
---
+++ src/App.vue
@@ -0,0 +1,264 @@
+<template>
+  <div id="app">
+    <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+    <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+  </div>
+</template>
+
+<script>
+import StyleEditor from "./components/StyleEditor";
+import ResumeEditor from "./components/ResumeEditor";
+import "./assets/reset.css";
+
+export default {
+  name: "app",
+  components: {
+    StyleEditor,
+    ResumeEditor
+  },
+  data() {
+    return {
+      // 动画延时
+      interval: 3,
+      currentStyle: "",
+      enableHtml: false,
+      fullStyle: [
+        `/*
+* Inspired by http://strml.net/
+* 源码地址 https://github.com/jimyfengqi/animores.git
+* 大家好，我是枫奇丶宛南。
+* 我来写一份简历！
+*/
+
+/* 给所有元素加上过渡效果 */
+* {
+  transition: all .1s;
+}
+/* 设置背景颜色 */
+html {
+  color: rgb(222,222,222); background: rgb(0,43,54);
+}
+
+/* 设置边框 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 45vw; height: 90vh;
+  /* background: rgb(20,20,20); */
+}
+/* 代码高亮 */
+.token.selector{ color: rgb(133,153,0); }
+.token.property{ color: rgb(187,137,0); }
+.token.punctuation{ color: yellow; }
+.token.function{ color: rgb(42,161,152); }
+
+/* 加3D效果 */
+html{
+  perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
+}
+
+/* 准备一个编辑器 */
+.resumeEditor{
+  position: fixed; right: 0; top: 0;
+  padding: .5em;  margin: .5em;
+  width: 48vw; height: 90vh;
+  border: 1px solid;
+  background: white; color: #222;
+  overflow: auto;
+}
+/* 开始写简历 */
+`,
+        `
+/*将Markdown格式翻译成HTML
+ *再对HTML加点样式
+*/
+.resumeEditor{
+  padding: 2em;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor ul,.resumeEditor ol{
+  list-style: none;
+}
+.resumeEditor ul> li::before{
+  content: '•';
+  margin-right: .5em;
+}
+.resumeEditor ol {
+  counter-reset: section;
+}
+.resumeEditor ol li::before {
+  counter-increment: section;
+  content: counters(section, ".") " ";
+  margin-right: .5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+`
+      ],
+      currentMarkdown: "",
+      fullMarkdown: `
+枫奇丶宛南
+----
+29岁，四年python 三年C 两年ruby 目前在北京从事后端工程师工作。
+
+
+- [中英文简历]()
+
+技能
+----
+* 后端开发(python为主)
+* Rails 开发
+* django 开发
+* 爬虫
+* Linux
+* 开源爱好者
+* 博客系统
+
+技术及语言
+----
+  - **后端**: django、flask、fastapi
+  - **前端**: Bootstrap、LayUI、React
+  - **数据库**: MySQL/MariaDB、MongoDB、redis、memcached
+  - **web 服务器**: Nginx
+  - **OS**: Linux、Windows
+  - **Others**: Git、Svn、XMind、Visio、IDEA
+
+工作经历
+----
+1. [中科创达软件股份有限公司](https://www.thundersoft.com/)
+2. [新拓尼克科技研发中心有限公司](https://www.syntronic.com/)
+
+开源项目
+----
+1. [基于hexo的主题bomboo](https://github.com/JimyFengqi/hexo-theme-bomboo)
+2. [一个会动的简历](https://github.com/JimyFengqi/animores.git)
+
+链接
+----
+* [个人博客](https://jimyfengqi.github.io/)
+* [GitHub](https://github.com/jimyfengqi)
+* [码云](https://gitee.com/jimyfengqi)
+* [博客园](https://www.cnblogs.com/aifengqi/)
+* [CSDN](https://blog.csdn.net/qiqiyingse)
+
+
+联系我
+----
+* 联系QQ：**215618733** | 邮箱：** jmps515@163.com **
+* 主要涉及技术：**python后端开发**、**爬虫**、**开源爱好者**、**Linux**
+
+> 如果你喜欢这个效果，Fork [我的项目](https://github.com/JimyFengqi/animores.git)，打造你自己的简历！
+`
+    };
+  },
+  created() {
+    this.makeResume();
+  },
+
+  methods: {
+    makeResume: async function() {
+      await this.progressivelyShowStyle(0);
+      await this.progressivelyShowResume();
+      await this.progressivelyShowStyle(1);
+      await this.showHtml();
+      await this.progressivelyShowStyle(2);
+    },
+    showHtml() {
+      return new Promise((resolve, reject) => {
+        this.enableHtml = true;
+        resolve();
+      });
+    },
+    progressivelyShowStyle(n) {
+      return new Promise((resolve, reject) => {
+        let interval = this.interval;
+        let showStyle = async function() {
+          let style = this.fullStyle[n];
+          if (!style) {
+            return;
+          }
+          // 计算前 n 个 style 的字符总数
+          let length = this.fullStyle
+            .filter((_, index) => index <= n)
+            .map(item => item.length)
+            .reduce((p, c) => p + c, 0);
+          let prefixLength = length - style.length;
+          if (this.currentStyle.length < length) {
+            let l = this.currentStyle.length - prefixLength;
+            let char = style.substring(l, l + 1) || " ";
+            this.currentStyle += char;
+            if (style.substring(l - 1, l) === "\n" && this.$refs.styleEditor) {
+              this.$nextTick(() => {
+                this.$refs.styleEditor.goBottom();
+              });
+            }
+            setTimeout(showStyle, interval);
+          } else {
+            resolve();
+          }
+        }.bind(this);
+        showStyle();
+      });
+    },
+    progressivelyShowResume() {
+      return new Promise((resolve, reject) => {
+        let length = this.fullMarkdown.length;
+        let interval = this.interval;
+        let showResume = () => {
+          if (this.currentMarkdown.length < length) {
+            this.currentMarkdown = this.fullMarkdown.substring(
+              0,
+              this.currentMarkdown.length + 1
+            );
+            let lastChar = this.currentMarkdown[
+              this.currentMarkdown.length - 1
+            ];
+            let prevChar = this.currentMarkdown[
+              this.currentMarkdown.length - 2
+            ];
+            if (prevChar === "\n" && this.$refs.resumeEditor) {
+              this.$nextTick(() => this.$refs.resumeEditor.goBottom());
+            }
+            setTimeout(showResume, interval);
+          } else {
+            resolve();
+          }
+        };
+        showResume();
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+html {
+  min-height: 100vh;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
